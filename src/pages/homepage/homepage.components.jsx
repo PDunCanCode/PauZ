f4d0653120a7d8273db4c3e8bf9900d9d7e1a523
Paul D. Duncan <chesiredarling@gmail.com>
2019-09-23 17:12:12 -0700

    ++ react-dom

--- src/pages/homepage/homepage.components.jsx
+++ src/pages/homepage/homepage.components.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
@@ -10,16 +10,3 @@ const HomePage = () => (
 );
 
 export default HomePage;
-=======
-import React from 'react';
-import Directory from '../../components/directory/directory.component';
-import './homepage.styles.scss';
-
-const HomePage = () => (
-    <div className= 'homepage'>
-        <Directory />
-      </div>
-);
-
-export default HomePage;
->>>>>>> 7cdd961f611b93cb23cde88d8a50d8ca77759754
