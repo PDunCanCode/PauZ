80ab60559147b03cfbc7d646e6fc8c171063eaee
Paul D. Duncan <chesiredarling@gmail.com>
2019-09-30 17:04:10 -0700

    cart.selectors, updated state

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -6,3 +6,14 @@ export const selectCartItems = createSelector(
     [selectCart],
     (cart) => cart.cartItems
 );
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => 
+    cartItems.reduce(
+        (accumalatedQuantity, cartItem) => 
+            accumalatedQuantity + cartItem.quantity, 
+        0
+    )
+
+);
