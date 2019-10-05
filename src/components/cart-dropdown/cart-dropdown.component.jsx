34d5151a0314c93f046ae6734334757efb83e774
Paul D. Duncan <chesiredarling@gmail.com>
2019-10-05 16:59:00 -0700

    toggling Cart cHeckouttt

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,7 +10,7 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown =({ cartItems, history }) => (
+const CartDropdown =({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {cartItem.length ? (
@@ -21,7 +21,13 @@ const CartDropdown =({ cartItems, history }) => (
                 <span className='empty-message'>Your cart is empty</span>
             )}
                 </div>
-        <CustomButton onClick={() => history.push('/checkout')}> GO TO CHECKOUT</CustomButton>
+        <CustomButton 
+            onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+        }}> 
+            GO TO CHECKOUT
+        </CustomButton>
     </div>
 );
 
