346e9032dee5615225b2e2343321dfe4cf1539ff
Paul D. Duncan <chesiredarling@gmail.com>
2019-10-07 21:48:31 -0700

    colleection updated

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,6 +1,14 @@
 
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+    hats: 1, 
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mes: 5,
+}
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
